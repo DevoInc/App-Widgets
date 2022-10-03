ef754e8f86abbc9d15d1bbc620a232b837770e00
Yair Kukielka <yair.kukielkazunzunegui@devo.com>
2022-10-03 13:31:30 +0200

    updated to webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,11 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/preset-scss',
+    '@storybook/addon-postcss',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
